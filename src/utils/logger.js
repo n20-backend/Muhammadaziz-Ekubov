fb5de18fa0ed382e113d8f671bd8d488db68fb17
Muhammad <muhammmadazizekubov@gmail.com>
2025-04-11 14:29:16 +0500

    done

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,16 +3,10 @@ import winston from "winston";
 const logger = winston.createLogger({
     level: "info",
     format: winston.format.combine(
-        winston.format.timestamp(),
         winston.format.json()
     ),
     transports: [
-        new winston.transports.Console({
-            format: winston.format.combine(
-                winston.format.colorize(),
-                winston.format.simple()
-            )
-        })
+        new winston.transports.Console()
     ]
 });
 
